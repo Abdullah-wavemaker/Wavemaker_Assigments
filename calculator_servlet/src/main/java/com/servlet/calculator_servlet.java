51e723234c3ad81107904ba0e9bc51411ed2ff30
Abdullah-wavemaker <124657961+Abdullah-wavemaker@users.noreply.github.com>
2023-02-18 11:19:20 +0530

    Update calculator_servlet.java

--- calculator_servlet/src/main/java/com/servlet/calculator_servlet.java
+++ calculator_servlet/src/main/java/com/servlet/calculator_servlet.java
@@ -9,12 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 public class calculator_servlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-            response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-
-        out.println("<html><head><title>Servlet CalculatorServlet</title></head><body>");               double n1 = Double.parseDouble(request.getParameter("txtN1"));
-
-
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Servlet CalculatorServlet</title></head><body>");    
+        double n1 = Double.parseDouble(request.getParameter("txtN1"));
         double n2 = Double.parseDouble(request.getParameter("txtN2"));
         double result =0;
         String opr=request.getParameter("operator");
