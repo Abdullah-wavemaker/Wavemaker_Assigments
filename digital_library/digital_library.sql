d63dde1412085ff6b24f2db6f996b8d9a11cde33
Abdullah-wavemaker <124657961+Abdullah-wavemaker@users.noreply.github.com>
2023-03-09 10:50:34 +0530

    Delete digital_library directory

--- digital_library/digital_library.sql
+++
@@ -1,4 +0,0 @@
-create database digital_library;
-use digital_library;
-create table books(book_id int primary key auto_increment,book_name varchar(30) unique,author_name varchar(30));
-insert into books values(1,"IN SEARCH OF LOST TIME","MARCEL PROUST"),(2,"ULYSSES","JAMES JOYCE");
